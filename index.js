b1c834e459bac557a169bb44a28192835564d16e
Ger Hobbelt <ger@hobbelt.com>
2020-05-10 10:12:28 +0200

    - use `'field' in <object>` check for options now: this fixes a subtle bug where the new merged feature implicitly flipped the default links setting from `true` to `undefined` == `false` when only a `{links: true}` options object was passed to this plugin. - lint fixes

--- index.js
+++ index.js
@@ -4,31 +4,41 @@ let container = require('@gerhobbelt/markdown-it-container');
 
 module.exports = function alerts_plugin(md, options) {
   let containerOpenCount = 0;
-  let links = options ? options.links : true;
-  var icons = options ? options.icons : false;
+  let links = 'links' in (options || {}) ? options.links : true;
+  let icons = 'icons' in (options || {}) ? options.icons : false;
+
+  function icon(name) {
+    switch (name) {
+    case 'warning':
+      return 'glyphicon glyphicon-alert';
+    case 'danger':
+      return 'glyphicon glyphicon-remove-sign';
+    default:
+      return 'glyphicon glyphicon-info-sign';
+    }
+  }
 
   function setupContainer(name) {
     md.use(container, name, {
       render: function (tokens, idx) {
-        var html;
+        let html;
         if (tokens[idx].nesting === 1) {
           containerOpenCount += 1;
           html = '<div class="alert alert-' + name + '" role="alert">\n';
           if (icons) {
             html += '<div class="alert-inner">\n';
             html += '<div class="alert-icon"><span class="' + icon(name) +
-              '" aria-hidden="true"></span></div>';
+              '" aria-hidden="true"></span></div>\n';
             html += '<div class="alert-message">';
           }
           return html;
-        } else {
-          containerOpenCount -= 1;
-          html = '</div>\n';
-          if (icons) {
-            html += '</div>\n</div>\n';
-          }
-          return html;
         }
+        containerOpenCount -= 1;
+        html = '</div>\n';
+        if (icons) {
+          html += '</div>\n</div>\n';
+        }
+        return html;
       }
     });
   }
@@ -46,22 +56,11 @@ module.exports = function alerts_plugin(md, options) {
 
     md.renderer.rules.link_open = function (tokens, idx, options, env, self) {
       if (isContainerOpen()) {
-        tokens[idx].attrPush(['class', 'alert-link']);
+        tokens[idx].attrPush([ 'class', 'alert-link' ]);
       }
 
       return defaultRender(tokens, idx, options, env, self);
     };
-  }
-
-  function icon(name) {
-    switch(name) {
-      case "warning":
-        return "glyphicon glyphicon-alert";
-      case "danger":
-        return "glyphicon glyphicon-remove-sign";
-      default:
-        return "glyphicon glyphicon-info-sign";
-    }
   }
 
   function init() {
