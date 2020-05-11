3dcbcf14cd0df41e053477c110f3659a97e180c2
Ger Hobbelt <ger@hobbelt.com>
2020-05-11 12:40:11 +0200

    synced with latest markdown-it toolchain

--- test/test.js
+++ test/test.js
@@ -1,10 +1,10 @@
 'use strict';
 
+/*eslint-env mocha*/
 
 let path     = require('path');
 let generate = require('@gerhobbelt/markdown-it-testgen');
 
-/*eslint-env mocha*/
 
 describe('markdown-it-alerts', function () {
   let md = require('@gerhobbelt/markdown-it')({ linkify: true })
